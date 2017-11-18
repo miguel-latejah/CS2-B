5e4243905c77adf21599f64b184056747592e4b6
miguel-latejah <33532873+miguel-latejah@users.noreply.github.com>
2017-11-18 14:01:17 -0800

    Update tictactoe-teach.py

--- tictactoe-teach.py
+++ tictactoe-teach.py
@@ -17,3 +17,25 @@ def drawBoard(board):
     print('   |   |')
     print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
     print('   |   |')
+def inputPlayerLetter():
+    #Lets the player enter which letter they want to play with
+    #Returns a list with the player's letter as the first item and the computer's letter as the second.
+    letter = ''
+    while not(letter == 'X' or letter == 'O'):
+        print('Do you want to be X or O?')
+        letter = input().upper()
+    #The first element in the list is the player's letter; the second is the computer's
+    if letter == 'X':
+        return ['X', 'O']
+    else:
+        return ['O', 'X']
+
+def whoGoesFirst():
+    #Randomly choose which player goes first
+    if random.randint(0,1) == 0:
+        return 'computer'
+    else:
+        return 'player'
+
+def makeMove(board, letter, move):
+    board[move] = letter
